0fcd1377c183c719d41d5677c5fb7743f7af0a6f
arnaud <songac.arnaud@gmail.com>
2017-01-23 14:26:46 -0500

    Properly set argument parser

---
+++ ArgumentParser.py
@@ -0,0 +1,46 @@
+'''
+Generate Argument Parsers for the client tool
+
+@author: arno
+'''
+
+import argparse
+
+def generateArgParsers():
+    parentParser = argparse.ArgumentParser(add_help=False, conflict_handler='resolve')
+    parentParser.add_argument('-v',
+                              dest="isVerbose",
+                              action="store_const", const=True, default=False,
+                              required=True,
+                              help="Prints the detail of the response such as protocol, status, and headers.")
+    parentParser.add_argument('-H',
+                              dest="headers",
+                              action="store",
+                              metavar="key:value",
+                              help="Associates headers to HTTP Request with the format 'key:value'")
+    
+    parentParser.add_argument('URL', action="store", help="URL for the Http request")
+    
+    # Main Parse: httpc client
+    mainParser = argparse.ArgumentParser(prog="httpc", description="httpc is a curl-like application but supports HTTP protocol only")
+    
+    subParsers = mainParser.add_subparsers(help='[command] help')
+    
+    # GET command
+    getParser = subParsers.add_parser('get',
+                                      parents=[parentParser],
+                                      help='Get executes a HTTP GET request for a given URL.')
+    
+    # POST command
+    postParser = subParsers.add_parser('post', 
+                                        parents=[parentParser],
+                                        epilog="Either [-d] or [-f] can be used but not both.",
+                                        help='Post executes a HTTP POST request for a given URL with inline data or from file.')
+    postParser.add_argument('-d', dest="data", action="store",
+                            metavar="inline-data",
+                            help="Associates an inline data to the body HTTP POST")
+    postParser.add_argument('-f', dest="file", action="store",
+                            metavar="file",
+                            help="Associates an inline data to the body HTTP POST")
+    return mainParser
+
--- httpPG.py
+++ httpPG.py
@@ -7,51 +7,46 @@ Created on Jan 21, 2017
 import socket
 import argparse
 import re
-from requests.api import request
+import ArgumentParser
 
 URL_REGEX = "^((http[s]?|ftp):\/)?\/?([^:\/\s\?]+)(:(\d+))?(\/([\w\/]+)(\.\w+)?)?(\?([\w=&]+))?$"
 CRLF = '\r\n'
 DEFAULT_PATH = "/"
 DEFAULT_PORT = 80
 
-# Usage: 
-parser = argparse.ArgumentParser()
-parser.add_argument('url', action="store")
-parser.add_argument('--post', '-P', dest="verb", action="store_const",
-                     const="POST", default="GET", help="HTTP Verb POST, or GET if not provided")
-parser.add_argument('--data', '-D', dest="data", action="store",
-                     help="Data to be included in a POST request")
+mainParser = ArgumentParser.generateArgParsers()
+args = mainParser.parse_args()
+print(args)
 
-args = parser.parse_args()
-url = args.url
-verb = args.verb
-data = args.data
-
-# Parse input 
-matcher = re.search(URL_REGEX, args.url)
-host = matcher.group(3)
-port = matcher.group(5)
-path = matcher.group(6)
-
-if port is None:
-    port = DEFAULT_PORT
-
-if path is None:
-    path = DEFAULT_PATH
-    
-
-headers = ''
-request = ''.join([verb, ' ', path, ' HTTP/1.1', CRLF, CRLF])
-
-if data is not None and verb == "POST":
-    request = ''.join([request, data])
-
-# Send Request
-print("Request to be sent:\r\n\r\n" + request)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                 
-s.connect((host , port))
-s.send(request.encode())
-print("\r\nRESPONSE:" + "\r\n")
-print(s.recv(4096))
-s.close    
+# url = args.url
+# verb = args.verb
+# data = args.data
+# 
+# # Parse input 
+# matcher = re.search(URL_REGEX, args.url)
+# host = matcher.group(3)
+# port = matcher.group(5)
+# path = matcher.group(6)
+# 
+# if port is None:
+#     port = DEFAULT_PORT
+# 
+# if path is None:
+#     path = DEFAULT_PATH
+#     
+# 
+# request = ''.join([verb, ' ', path, ' HTTP/1.1', CRLF, CRLF])
+# 
+# if data is not None and verb == "POST":
+#     request = ''.join([request, data])
+#     headers = ''.join(['Content-Type'])
+# 
+# # Send Request
+# print("Request to be sent:\r\n\r\n" + request)
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                 
+# s.connect((host , port))
+# s.send(request.encode())
+# print("\r\nRESPONSE:" + "\r\n")
+# print(s.recv(4096))
+# s.close    
 
