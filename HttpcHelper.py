b034934294b23c5345191fd55015dc1f035c7466
arnaud <songac.arnaud@gmail.com>
2017-01-30 15:59:45 -0500

    Implemented a class to handle the request elements

---
+++ HttpcHelper.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python3
+'''
+Created on Jan 21, 2017
+
+@author: arno
+'''
+
+import socket
+import re
+import sys
+
+
+POST = "POST"
+GET = "GET"
+URL_REGEX = "^((http[s]?|ftp):\/)?\/?((www\.)?([^:\/\s\?]+))(:(\d+))?(\/([\w\/]+)(\.\w+)?)?(\?([\w=&]+))?$"
+CRLF = '\r\n'
+DEFAULT_PATH = "/"
+DEFAULT_PORT = 80
+contentType = "Content-Type: {content_type}"
+contentLength = "Content-length: {content_length}"
+hostName = "Host: {host_name}"
+CONNECTION_CLOSE = "Connection: Keep-Alive"
+
+class HttpRequest:
+    
+    def __init__(self, url, verb, headers, host, port,
+                  path, data, file, verbose, outputFile):
+        self.verb = verb
+        self.headers = headers
+        self.host = host
+        self.port = DEFAULT_PORT
+        if port is not None :
+            self.port = port
+        self.path = DEFAULT_PATH
+        if path is not None :
+            self.path = path
+        self.data = data
+        self.file = file
+        self.verbose = verbose
+        self.outputFile = outputFile
+        return
+        
+        
+        
+    # #######################
+    # Build HTTP request
+    # ####################### 
+    def buildRequest(self):
+    
+        headersJson = {}
+        
+        if self.headers is not None:
+            for header in self.headers:
+                splittedHeader = header.split(':')
+                headersJson[splittedHeader[0]]  = splittedHeader[1]
+        
+        request = ''.join([self.verb, ' ', self.path, ' HTTP/1.1', CRLF])
+        
+        request = ''.join([request,
+                           hostName.format(host_name = str(self.host)), CRLF,
+                           CONNECTION_CLOSE, CRLF, CRLF
+                           ])
+        
+        if self.verb == POST:
+            parameters = ''
+            if self.file is None:
+                parameters = self.data
+            else:
+                f = open(self.file, 'r')
+                for line in f:
+                    if line:
+                        parameters += line.rstrip('\n') + '&'
+        
+            if parameters[-1] == '&':
+                parameters = parameters.rstrip('&')
+        
+            data_bytes = parameters.encode()
+            request = ''.join([request,
+                               contentLength.format(content_length = len(data_bytes)), CRLF,
+                               parameters])
+            
+        request = ''.join([request, CRLF])
+        
+        if headersJson:
+            for key, value in headersJson.items():
+                request = ''.join([request, key,  ':' , value , CRLF])
+                
+        
+        self.request = request
+        return
+        
+        
+        
+        
+        
+        
+    # #######################
+    # Process Http Request
+    # ####################### 
+    def processRequest(self):
+        
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # SOCK_STREAM for TCP
+            s.connect((self.host , self.port))
+            s.send(self.request.encode())
+            responseBytes = s.recv(4096)
+            response = responseBytes.decode()
+        except:
+            print("An error occurred, please retry: ", sys.exc_info())
+            sys.exit(0)
+        
+        
+        responseArr = response.split(CRLF+CRLF)
+        responseHeaders = responseArr[0]
+        responseHeadersArr = responseHeaders.split(CRLF)
+        responseStatus = parseResonseStatus(responseHeaders)
+        
+        if responseStatus >= 300 and responseStatus < 400 :
+            newUrl = parseRedirectUrlFor300(response)
+            matcher = re.search(URL_REGEX, newUrl)
+            self.host = matcher.group(5)
+            self.port = matcher.group(7)
+            if self.port is None:
+                self.port = DEFAULT_PORT
+                
+            self.path = matcher.group(8)
+            if self.path is None :
+                self.path = DEFAULT_PATH
+            print(responseHeadersArr[0])
+            print("Redirect url: ", newUrl)
+            answer = input("Do you accept to follow the redirection link? [Y/n]: ")
+            if answer != 'n' :
+                self.buildRequest()
+                self.processRequest()
+            
+        else :
+            finalOutput = ''
+            if self.verbose:
+                finalOutput = responseArr[0]
+                
+            finalOutput = ''.join([finalOutput, CRLF, CRLF, responseArr[1]])
+            
+            if self.outputFile is None :
+                print(finalOutput)
+            else :
+                f = open(self.outputFile, 'w')
+                f.write(finalOutput)
+                f.close()
+        
+        return
+
+    
+
+
+# #######################
+# Extract response status
+# ####################### 
+def parseResonseStatus(responseHeaders):
+    responseHeadersArr = responseHeaders.split(CRLF)
+    responseStatus = int(responseHeadersArr[0].split(' ')[1])
+    return responseStatus
+
+# #######################
+# Extract redirect url
+# ####################### 
+def parseRedirectUrlFor300(responseBody):
+    #TODO
+    return "http://httpbin.org/get"
+    
+
+
+
+
+
+
+
--- httpc.py
+++ httpc.py
@@ -5,148 +5,11 @@ Created on Jan 21, 2017
 @author: arno
 '''
 
-import socket
 import re
 import ArgumentParser
-import sys
+import HttpcHelper
+from HttpcHelper import HttpRequest
 
-POST = "POST"
-GET = "GET"
-URL_REGEX = "^((http[s]?|ftp):\/)?\/?((www\.)?([^:\/\s\?]+))(:(\d+))?(\/([\w\/]+)(\.\w+)?)?(\?([\w=&]+))?$"
-CRLF = '\r\n'
-DEFAULT_PATH = "/"
-DEFAULT_PORT = 80
-contentType = "Content-Type: {content_type}"
-contentLength = "Content-length: {content_length}"
-hostName = "Host: {host_name}"
-CONNECTION_CLOSE = "Connection: Keep-Alive"
-
-
-# #######################
-# Extract response status
-# ####################### 
-def parseResonseStatus(responseHeaders):
-    responseHeadersArr = responseHeaders.split(CRLF)
-    responseStatus = int(responseHeadersArr[0].split(' ')[1])
-    return responseStatus
-
-# #######################
-# Extract redirect url
-# ####################### 
-def parseRedirectUrlFor300(responseBody):
-    #TODO
-    return "http://httpbin.org/get"
-    
-# #######################
-# Build HTTP request
-# ####################### 
-def buildRequest(url, verb, host, port, path, data, file):
-
-    headersJson = {}
-    
-    if headers is not None:
-        for header in headers:
-            splittedHeader = header.split(':')
-            headersJson[splittedHeader[0]]  = splittedHeader[1]
-    
-    if port is None:
-        port = DEFAULT_PORT
-     
-    if path is None:
-        path = DEFAULT_PATH
-         
-    request = ''.join([verb, ' ', path, ' HTTP/1.1', CRLF])
-    
-    request = ''.join([request,
-                       hostName.format(host_name = str(host)), CRLF,
-                       CONNECTION_CLOSE, CRLF, CRLF
-                       ])
-    
-    if verb == POST:
-        parameters = ''
-        if file is None:
-            parameters = data
-        else:
-            f = open(file, 'r')
-            for line in f:
-                if line:
-                    parameters += line.rstrip('\n') + '&'
-    
-        if parameters[-1] == '&':
-            parameters = parameters.rstrip('&')
-    
-        data_bytes = parameters.encode()
-        request = ''.join([request,
-                           contentLength.format(content_length = len(data_bytes)), CRLF,
-                           parameters])
-        
-    request = ''.join([request, CRLF])
-    
-    if headersJson:
-        for key, value in headersJson.items():
-            request = ''.join([request, key,  ':' , value , CRLF])
-            
-    
-    return request
-
-# #######################
-# Process Http Request
-# ####################### 
-def processRequest(request, verb, host, port, data, file, verbose, outputFile):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host , port))
-        s.send(request.encode())
-        responseBytes = s.recv(4096)
-        response = responseBytes.decode()
-    except:
-        print("An error occurred, please retry: ", sys.exc_info())
-        sys.exit(0)
-    
-    
-    responseArr = response.split(CRLF+CRLF)
-    responseHeaders = responseArr[0]
-    responseHeadersArr = responseHeaders.split(CRLF)
-    responseStatus = parseResonseStatus(responseHeaders)
-    
-    if responseStatus >= 300 and responseStatus < 400 :
-        newUrl = parseRedirectUrlFor300(response)
-        matcher = re.search(URL_REGEX, newUrl)
-        host = matcher.group(5)
-        port = matcher.group(7)
-        if port is None:
-            port = DEFAULT_PORT
-            
-        path = matcher.group(8)
-        if path is None :
-            path = DEFAULT_PATH
-        print(responseHeadersArr[0])
-        print("Redirect url: ", newUrl)
-        answer = input("Do you accept to follow the redirection link? [Y/n]: ")
-        if answer != 'n' :
-            newRequest = buildRequest(newUrl, verb, host, port, path, data, file)
-            processRequest(newRequest, verb, host, port, data, file, verbose, outputFile)
-        
-    else :
-        finalOutput = ''
-        if verbose:
-            finalOutput = responseArr[0]
-            
-        finalOutput = ''.join([finalOutput, CRLF, CRLF, responseArr[1]])
-        
-        if outputFile is None :
-            print(finalOutput)
-        else :
-            f = open(outputFile, 'w')
-            f.write(finalOutput)
-            f.close()
-    
-    return
-
-
-# #######################
-# Execution
-# ####################### 
 
 mainParser = ArgumentParser.generateArgParsers()
 args = mainParser.parse_args()
@@ -156,24 +19,21 @@ headers = args.headers
 verb = args.subparser_name.upper()
 verbose = args.isVerbose
 outputFile = args.outputFile
-if verb == POST:
+if verb == HttpcHelper.POST:
     data = args.data
     file = args.file
 else:
     data = file = None
 
-matcher = re.search(URL_REGEX, url)
+matcher = re.search(HttpcHelper.URL_REGEX, url)
 host = matcher.group(5)
 port = matcher.group(7)
-if port is None:
-    port = DEFAULT_PORT
-    
 path = matcher.group(8)
-if path is None :
-    path = DEFAULT_PATH
 
-request = buildRequest(url, verb, host, port, path, data, file)
-processRequest(request, verb, host, port, data, file, verbose, outputFile)
+
+httpRequest = HttpRequest(url, verb, headers, host, port, path, data, file, verbose, outputFile)
+httpRequest.buildRequest()
+httpRequest.processRequest()
 
 
 
